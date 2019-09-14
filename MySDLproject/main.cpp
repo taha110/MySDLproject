dce1a771d0158853e1e47311a1d01746a368bee7
Taha Rizvi <taha.hassan110@gmail.com>
2019-09-14 12:40:29 +0500

    hello SDL world !
    setting up window

--- MySDLproject/main.cpp
+++ MySDLproject/main.cpp
@@ -1,18 +1,40 @@
 #include "SDL.h"
 
-int main(int argc, char* argv[]) {
+SDL_Window* g_pWindow = 0;
+SDL_Renderer* g_pRenderer = 0;
 
-	SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_Window* window = SDL_CreateWindow("myTitle", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 480, SDL_WINDOW_SHOWN);
-	SDL_Renderer* renderer = SDL_CreateRenderer(window , -1, 0);
+int main(int argc, char* args[])
+{
+	// initialize SDL
+	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
+	{
+		// if succeeded create our window
+		g_pWindow = SDL_CreateWindow("Chapter 1: Setting up SDL",
+			SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+			640, 480,
+			SDL_WINDOW_SHOWN);
+		// if the window creation succeeded create our renderer
+		if (g_pWindow != 0)
+		{
+			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+		}
+	}
+	else
+	{
+		return 1; // sdl could not initialize
+	}
+	// everything succeeded lets draw the window
+	// set to black // This function expects Red, Green, Blue and
+	// Alpha as color values
+	SDL_SetRenderDrawColor(g_pRenderer, 0, 200, 0, 255);
+	// clear the window to black
+	SDL_RenderClear(g_pRenderer);
+	// show the window
 
-	SDL_SetRenderDrawColor(renderer, 0,255,0,255);
-
-	SDL_RenderClear(renderer);
-
-	SDL_RenderPresent(renderer);
-
+	SDL_RenderPresent(g_pRenderer);
+	// set a delay before quitting
 	SDL_Delay(3000);
-
+	// clean up SDL
+	SDL_Quit();
 	return 0;
 }
